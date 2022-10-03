4a3197e20ef97fc1f381e89a59e3ce8ebb14913c
Sachin Panchal <mrsachinpanchal8@gmail.com>
2022-10-03 15:49:01 +0100

    Update rockPaperScissors.py

--- 100_Days_Of_Code/Section_4_Python_Lists_Randomisation/rockPaperScissors.py
+++ 100_Days_Of_Code/Section_4_Python_Lists_Randomisation/rockPaperScissors.py
@@ -39,16 +39,16 @@ if user_choice >= 3 or user_choice < 0:
     print("Invalid Choice. You Loose!")
 else:
     print(result_list[user_choice])
-    print("Computer chose:")
+    print(f"Computer chose: {computer_choice}")
     print(result_list[computer_choice])
 
     if user_choice == 0 and computer_choice == 2:
         print("You Win!")
     elif computer_choice == 2 and user_choice == 0:
         print("You Loose!")
-    elif user_choice > computer_choice:
-        print("You Win!")
     elif computer_choice > user_choice:
         print("You Loose!")
+    elif user_choice > computer_choice:
+        print("You Win!")
     elif computer_choice == user_choice:
         print("It's a draw")
